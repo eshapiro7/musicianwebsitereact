7775b9992d788aa88d152376cd60ed8b7aaf183d
Elizabeth Shapiro <elizabethashapiro@gmail.com>
2022-03-27 13:54:57 -0400

    gig array

---
+++ src/giglist.js
@@ -0,0 +1,33 @@
+export const GIGLIST = [
+    {
+        id: 0, 
+        date: '6/22/22',
+        location: 'Greenroom 42 New York, NY',
+        ticket: 'https://thegreenroom42.venuetix.com/show/details/ZCoquGtfA7zvtf2l3cQf/1655938800000'
+    },
+    {
+        id: 1, 
+        date: '6/25/22',
+        location: 'Greenroom 42 New York, NY',
+        ticket: 'https://thegreenroom42.venuetix.com/show/details/ZCoquGtfA7zvtf2l3cQf/1655938800000'
+    },
+    {
+        id: 2, 
+        date: '7/3/22',
+        location: 'Anything Brooklyn, NY',
+        ticket: 'https://thegreenroom42.venuetix.com/show/details/ZCoquGtfA7zvtf2l3cQf/1655938800000'
+    },
+    {
+        id:3, 
+        date: '7/9/22',
+        location: 'Anything Brooklyn, NY',
+        ticket: 'https://thegreenroom42.venuetix.com/show/details/ZCoquGtfA7zvtf2l3cQf/1655938800000'
+    },
+    {
+        id: 4, 
+        date: '7/30/22',
+        location: 'Lips Cafe Brooklyn, NY',
+        ticket: 'https://thegreenroom42.venuetix.com/show/details/ZCoquGtfA7zvtf2l3cQf/1655938800000'
+    }
+    
+];
